731e4ecccc14a2d180f9a4b7d92dc9f2f31a9b9e
Andrey Ivanov <prioritet2503@gmail.com>
2022-09-16 01:04:18 +0300

    save

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Container } from './Container/Container';
+export { default as NavBar } from './NavBar/NavBar';
+export { default as AddContactsForm } from './AddContactsForm/AddContactsForm';
+export { default as ContactsList } from './ContactsList/ContactsList';
+export { default as Filter } from './Filter/Filter';
+export { default as ContactItem } from './ContactItem/ContactItem';
+export { default as Loader } from './Loader/Loader';
