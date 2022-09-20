077c61887c140300088edfd79059a7dbbf3050db
Andrey Ivanov <prioritet2503@gmail.com>
2022-09-21 02:27:26 +0300

    save

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 600px;
+  max-width: 600px;
   height: 100%;
   padding: 0 15px;
   margin: 0 auto;
