644e258ebd3816a59de3f591523e1eb405230861
Andrey Ivanov <prioritet2503@gmail.com>
2022-09-21 12:16:42 +0300

    save

--- src/components/FixedBox/FixedBox.js
+++ src/components/FixedBox/FixedBox.js
@@ -138,11 +138,21 @@ const ResponsiveAppBar = () => {
             </Box>
 
             <Box sx={{ flexGrow: 0 }}>
+              <Button sx={{ bgcolor: '#708fad' }} variant="contained">
+                Register
+              </Button>
+              <Button sx={{ ml: 2, bgcolor: '#708fad' }} variant="contained">
+                Login
+              </Button>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
               <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                   <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                 </IconButton>
               </Tooltip>
+
               <Menu
                 sx={{ mt: '45px' }}
                 id="menu-appbar"
